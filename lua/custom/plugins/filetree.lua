c0955222212b5d444b1c1310da1796d779edd6d0
Kedar Shinde <kedarshinde8011@gmail.com>
2024-01-04 10:31:46 +0530

    Initial Commit

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,18 @@
+-- Unless you are still migrating, remove the deprecated commands from v1.x
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  keys={
+    {'<leader>ft', ':Neotree toggle<CR>', {desc="neotree toggle"}}
+  },
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  config = function ()
+    require('neo-tree').setup {}
+  end,
+}
